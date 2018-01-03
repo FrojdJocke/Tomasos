8211698368e68e02cd3ceb5f19dd562e8cd2ec97
Joakim Fröjd <frojdie@gmail.com>
2018-01-03 15:44:07 +0100

    Skapat Body samt header med Modal.

--- Models/Kund.cs
+++ Models/Kund.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TomasosASP.Models
 {
@@ -11,13 +12,18 @@ namespace TomasosASP.Models
         }
 
         public int KundId { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i ditt namn")]
         public string Namn { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i en adress")]
         public string Gatuadress { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i ett postnummer")]
         public string Postnr { get; set; }
         public string Postort { get; set; }
         public string Email { get; set; }
         public string Telefon { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i ett användarnamn")]
         public string AnvandarNamn { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i ett lösenord")]
         public string Losenord { get; set; }
 
         public ICollection<Bestallning> Bestallning { get; set; }
